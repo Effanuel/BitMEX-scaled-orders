3348dc6e76c62cfa430eb15ea2080e2b5c449041
Effanuel <etajuozas@gmail.com>
2019-08-24 21:54:17 +0300

    dotenv wasnt being loaded properly

--- api/index.js
+++ api/index.js
@@ -1,35 +1,36 @@
-const express = require("express"),
+require('dotenv').config();
+
+const express = require('express'),
   app = express(),
-  cors = require("cors"),
-  bodyParser = require("body-parser"),
-  request = require("request"),
-  adminRoutes = require("./routes/admin"),
-  path = require("path");
+  cors = require('cors'),
+  bodyParser = require('body-parser'),
+  request = require('request'),
+  bitmexRoutes = require('./routes/bitmex'),
+  path = require('path');
 
 const port = process.env.PORT || 3001;
-require("dotenv").config();
 
 app.use(cors());
-app.disable("etag").disable("x-powered-by");
+app.disable('etag').disable('x-powered-by');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("/admin", adminRoutes);
+app.use('/bitmex', bitmexRoutes);
 
 //require('./routes')(app);
 
-if (process.env.NODE_ENV != "development ") {
+if (process.env.NODE_ENV != 'development ') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   // Handle React routing, return all requests to React app
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
 app.listen(port, () => {
-  console.log("running on: " + port);
+  console.log(`Server running on: htpp://localhost:${port}`);
 });
 
 module.exports = app;
