a01f622c867314109d2940d431e1462bee4fdc3d
Effanuel <etajuozas@gmail.com>
2019-08-24 21:53:58 +0300

    added positive/negative distributions

--- client/src/util/index.js
+++ client/src/util/index.js
@@ -1,31 +1,17 @@
-const startEndPutOrders = (obj, start, end, amount, side) => {
-  obj.push({
-    symbol: 'XBTUSD',
-    side: side,
-    orderQty: amount,
-    price: start,
-    ordType: 'Limit',
-    execInst: 'ParticipateDoNotInitiate',
-    text: 'order'
-  });
-  obj.push({
-    symbol: 'XBTUSD',
-    side: side,
-    orderQty: amount,
-    price: end,
-    ordType: 'Limit',
-    execInst: 'ParticipateDoNotInitiate',
-    text: 'order'
-  });
-};
 const roundHalf = num => {
   return Math.round(num * 2) / 2;
 };
+
+const gaussian = (mean, x, delta) => {
+  const member1 = 1 / (delta * Math.sqrt(2 * Math.PI));
+  const member2 = Math.pow(Math.E, -((x - mean) ** 2) / (2 * delta ** 2));
+  return member1 * member2;
+};
+
 const Uniform = (amount, n_tp, start, end, side, symbol) => {
   let orders = { orders: [] };
 
   const increment = roundHalf((end - start) / (n_tp - 1));
-  console.log(increment, 'INCREMENTS');
   const mean = Math.floor(amount / n_tp);
   //startEndPutOrders(orders.orders, start, end, mean, side);
   for (let i = 0; i < n_tp; i++) {
@@ -44,23 +30,6 @@ const Uniform = (amount, n_tp, start, end, side, symbol) => {
   return orders;
 };
 const Positive = (amount, n_tp, start, end, side, symbol) => {
-  let orders = { orders: [] };
-  const increment = Math.round((end - start) / n_tp);
-
-  return orders;
-};
-const Negative = (amount, n_tp, start, end, side, symbol) => {
-  let orders = { orders: [] };
-  const increment = Math.round((end - start) / n_tp);
-
-  return orders;
-};
-const Normal = (amount, n_tp, start, end, side, symbol) => {
-  const gauss = (mean, x, delta) => {
-    const member1 = 1 / (delta * Math.sqrt(2 * Math.PI));
-    const member2 = Math.pow(Math.E, -((x - mean) ** 2) / (2 * delta ** 2));
-    return member1 * member2;
-  };
   const START_CFG = -2;
   const END_CFG = 2;
 
@@ -68,7 +37,65 @@ const Normal = (amount, n_tp, start, end, side, symbol) => {
 
   const arr = [];
   for (let i = 0; i < n_tp; i++) {
-    arr.push(gauss(0, START_CFG + i * incrementQty, 1)); //mean == 0
+    arr.push(gaussian(-1, START_CFG + i * incrementQty, 1)); //mean == -1
+  }
+  const summ = arr.reduce((a, b) => a + b, 0);
+
+  const incrementPrice = (end - start) / (n_tp - 1);
+  let orders = { orders: [] };
+  for (let i = 0; i < n_tp; i++) {
+    //ROUND TO NEAREST 0.5
+    orders.orders.push({
+      symbol: symbol,
+      side: side,
+      orderQty: Math.floor((arr[i] / summ) * amount),
+      price: roundHalf(start + i * incrementPrice),
+      ordType: 'Limit',
+      execInst: 'ParticipateDoNotInitiate',
+      text: 'order'
+    });
+  }
+
+  return orders;
+};
+const Negative = (amount, n_tp, start, end, side, symbol) => {
+  const START_CFG = -2;
+  const END_CFG = 2;
+
+  const incrementQty = (END_CFG - START_CFG) / (n_tp - 1);
+
+  const arr = [];
+  for (let i = 0; i < n_tp; i++) {
+    arr.push(gaussian(1, START_CFG + i * incrementQty, 1)); //mean == 1
+  }
+  const summ = arr.reduce((a, b) => a + b, 0);
+
+  const incrementPrice = (end - start) / (n_tp - 1);
+  let orders = { orders: [] };
+  for (let i = 0; i < n_tp; i++) {
+    //ROUND TO NEAREST 0.5
+    orders.orders.push({
+      symbol: symbol,
+      side: side,
+      orderQty: Math.floor((arr[i] / summ) * amount),
+      price: roundHalf(start + i * incrementPrice),
+      ordType: 'Limit',
+      execInst: 'ParticipateDoNotInitiate',
+      text: 'order'
+    });
+  }
+
+  return orders;
+};
+const Normal = (amount, n_tp, start, end, side, symbol) => {
+  const START_CFG = -2;
+  const END_CFG = 2;
+
+  const incrementQty = (END_CFG - START_CFG) / (n_tp - 1);
+
+  const arr = [];
+  for (let i = 0; i < n_tp; i++) {
+    arr.push(gaussian(0, START_CFG + i * incrementQty, 1)); //mean == 0
   }
   const summ = arr.reduce((a, b) => a + b, 0);
 
