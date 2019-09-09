364696d8c86de491cb2340d5708f527accb63fce
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-09-09 19:53:42 +0300

    Missing Loading for redux

--- client/src/redux/actions/previewActions.js
+++ client/src/redux/actions/previewActions.js
@@ -3,6 +3,7 @@ import {
   // POST_ORDER, //IGNORING LOADERS FOR NOW
   POST_ORDER_SUCCESS,
   POST_ORDER_ERROR,
+  POST_ORDER_LOADING,
   // PREVIEW_PRICE, //IGNORING LOADERS FOR NOW
   // PREVIEW_ORDERS, //IGNORING LOADERS FOR NOW
   PREVIEW_ORDERS_SUCCESS
@@ -16,12 +17,20 @@ import { orderBulk } from "../../util";
 export const postOrder = pay => async dispatch => {
   try {
     // console.log(payload, "post order payload 112");
+    dispatch(postOrderLoading());
     const payload = orderBulk(pay);
     const response = await axios.post("/bitmex/postOrder", payload);
     dispatch(postOrderSuccess(response.data));
   } catch (err) {
     dispatch(postOrderError(err.response.data));
   }
+};
+
+const postOrderLoading = payload => {
+  return {
+    type: POST_ORDER_LOADING,
+    payload
+  };
 };
 
 /**
--- client/src/redux/reducers/previewReducer.js
+++ client/src/redux/reducers/previewReducer.js
@@ -2,6 +2,7 @@ import {
   POST_ORDER,
   POST_ORDER_SUCCESS,
   POST_ORDER_ERROR,
+  POST_ORDER_LOADING,
   PREVIEW_ORDERS_SUCCESS
 } from "../actions/actionTypes";
 
@@ -15,10 +16,25 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case POST_ORDER_LOADING:
+      return { ...state, error: "", loading: true };
     case POST_ORDER_SUCCESS:
-      return { ...state, ...payload, showPreview: false, error: "", order: [] };
+      return {
+        ...state,
+        ...payload,
+        showPreview: false,
+        error: "",
+        order: [],
+        loading: false
+      };
     case POST_ORDER_ERROR:
-      return { ...state, showPreview: false, orders: [], error: payload };
+      return {
+        ...state,
+        showPreview: false,
+        orders: [],
+        error: payload,
+        loading: false
+      };
     case PREVIEW_ORDERS_SUCCESS:
       return { ...state, showPreview: true, orders: payload.orders, error: "" };
     default:
