98aa7c125a70aa80fec7620ce4d9cf30a4af9f42
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-09-09 21:24:46 +0300

    Added average price to preview table

--- client/src/components/OrdersPreviewTable/index.js
+++ client/src/components/OrdersPreviewTable/index.js
@@ -3,10 +3,17 @@ import { Table } from "react-bootstrap";
 import { connect } from "react-redux";
 import styles from "./OrdersPreviewTable.module.css";
 
-const OrdersPreviewTable = ({ preview }) => {
+import { ordersAveragePriceSelector } from "../../redux/selectors";
+
+const OrdersPreviewTable = ({ preview, averagePrice }) => {
   return (
-    <Table className={styles.myTable} striped bordered variant="dark" size="sm">
+    <Table className={styles.myTable} striped variant="dark" size="sm">
       <thead>
+        <tr>
+          <th colSpan={3}>
+            Average price: <d className={styles.customStyle}>{averagePrice}</d>
+          </th>
+        </tr>
         <tr>
           <th>Quantity</th>
           <th>Side</th>
@@ -18,7 +25,12 @@ const OrdersPreviewTable = ({ preview }) => {
           return (
             <tr key={String(i)}>
               <td key={x * i + "a"}>{x.orderQty}</td>
-              <td key={x * i + "b"}>{x.side}</td>
+              <td
+                key={x * i + "b"}
+                style={{ color: x.side === "Sell" ? "#d50000" : "#00ca45" }}
+              >
+                {x.side}
+              </td>
               <td key={x * i + "c"}>{x.price}</td>
             </tr>
           );
@@ -29,7 +41,8 @@ const OrdersPreviewTable = ({ preview }) => {
 };
 
 const mapStateToProps = state => ({
-  preview: state.preview
+  preview: state.preview,
+  averagePrice: ordersAveragePriceSelector(state)
 });
 export default connect(
   mapStateToProps,
--- client/src/redux/selectors/index.js
+++ client/src/redux/selectors/index.js
@@ -3,6 +3,7 @@ import { createSelector } from "reselect";
 const getShowPreview = state => state.preview.showPreview;
 const getError = state => state.preview.error;
 const websocketData = state => state.websocket.data;
+const getOrders = state => state.preview.orders;
 
 export const showPreviewSelector = createSelector(
   [getShowPreview],
@@ -18,5 +19,22 @@ export const websocketDataSelector = createSelector(
   [websocketData],
   data => {
     return data.action === "insert" ? data.data[0].askPrice : "";
+  }
+);
+
+export const ordersAveragePriceSelector = createSelector(
+  [getOrders, getShowPreview],
+  (orderList, previewTable) => {
+    if (previewTable) {
+      const total_quantity = orderList.reduce(
+        (total, n) => total + n.orderQty,
+        0
+      );
+      const contract_value = orderList.reduce(
+        (total, n) => total + n.orderQty / n.price,
+        0
+      );
+      return Math.round((total_quantity / contract_value) * 2) / 2;
+    }
   }
 );
