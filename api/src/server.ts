bb4173c75395e58c2631a59cb01212f3665e17d0
Effanuel <etajuozas@gmail.com>
2019-10-25 21:43:26 +0300

    Unnecessary code

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,17 +1,10 @@
-import app from "./app";
-import process from "process";
-const server = app.listen(app.get("port"), () => {
-  console.log(
-    "  App is running at http://localhost:%d in %s mode",
-    app.get("port"),
-    app.get("env")
-  );
-  console.log("  Press CTRL-C to stop\n");
-});
-
-process.on("SIGTERM", function() {
-  server.close(function() {
-    process.exit(0);
+import app from './app';
+import process from 'process';
+const server = app.listen(app.get('port'), () => {
+  process.on('SIGTERM', function() {
+    server.close(function() {
+      process.exit(0);
+    });
   });
 });
 
