19896cdcb3859a58da8e7e53cf794f14cc6037e3
Effanuel <32594043+Effanuel@users.noreply.github.com>
2019-12-31 03:33:46 +0200

    Removed ccxt dependency for postOrder

--- api/src/controllers/rangeToolAPI.ts
+++ api/src/controllers/rangeToolAPI.ts
@@ -1,44 +1,47 @@
 // const request = require('request');
 // const crypto = require('crypto');
-import * as ccxt from "ccxt";
+// import * as ccxt from "ccxt";
 import { Request, Response } from "express";
+import { _curl_bitmex } from "../util/auth";
 
-const exchangeId = "bitmex",
-  exchangeClass = ccxt[exchangeId],
-  exchange = new exchangeClass({
-    apiKey: process.env.API_KEY,
-    secret: process.env.API_SECRET,
-    timeout: 30000,
-    enableRateLimit: process.env.ENABLE_RATE_LIMIT == "true"
-  });
-if (process.env.TESTNET == "true") {
-  exchange.urls["api"] = exchange.urls["test"];
-}
+// const exchangeId = "bitmex",
+//   exchangeClass = ccxt[exchangeId],
+//   exchange = new exchangeClass({
+//     apiKey: process.env.API_KEY,
+//     secret: process.env.API_SECRET,
+//     timeout: 30000,
+//     enableRateLimit: process.env.ENABLE_RATE_LIMIT == "true"
+//   });
+// if (process.env.TESTNET == "true") {
+//   exchange.urls["api"] = exchange.urls["test"];
+// }
 
-const errorHandling = (e: any, res: any) => {
-  const errorMessage = JSON.parse(e.message.slice(7)).error.message;
+// const errorHandling = (e: any, res: any) => {
+//   const errorMessage = JSON.parse(e.message.slice(7)).error.message;
 
-  if (e instanceof ccxt.NetworkError) {
-    return res.status(500).json({ errorMessage });
-  } else if (e instanceof ccxt.ExchangeError) {
-    return res.status(500).json({ errorMessage });
-  } else if (e instanceof ccxt.RequestTimeout) {
-    //validation error *
-    return res.status(500).json({ errorMessage });
-  } else {
-    return res.status(500).json({ errorMessage: "ERROR" });
-  }
-};
+//   if (e instanceof ccxt.NetworkError) {
+//     return res.status(500).json({ errorMessage });
+//   } else if (e instanceof ccxt.ExchangeError) {
+//     return res.status(500).json({ errorMessage });
+//   } else if (e instanceof ccxt.RequestTimeout) {
+//     //validation error *
+//     return res.status(500).json({ errorMessage });
+//   } else {
+//     return res.status(500).json({ errorMessage: "ERROR" });
+//   }
+// };
 
 /**
  * Bulk order POST request to the exchange API
  */
 export const postOrder = async (req: Request, res: Response, next: any) => {
   try {
-    const response = await exchange.privatePostOrderBulk(req.body);
+    // const response = await exchange.privatePostOrderBulk(req.body);
+    const response = await _curl_bitmex("order/bulk", req.body, "POST");
+    console.log("POST", response);
     return res.send({ success: res.statusCode });
   } catch (error) {
-    errorHandling(error, res);
+    return res.status(400).send({ error: error });
   }
 };
 // exports.getInstruments = async (req, res, next) => {
@@ -89,34 +92,9 @@ export const postOrder = async (req: Request, res: Response, next: any) => {
 //     return res.send({ instruments: instrumentsArray });
 //   });
 // };
-
-// function putResponse(data, path, method) {
-//   //path_leverage = '/api/v1/position/leverage', //POST /position/leverage
-//   let expires = Math.round(new Date().getTime() / 1000) + 60; // 1 min in the future
-//   let postBody = JSON.stringify(data);
-//   let signature = crypto
-//     .createHmac('sha256', process.env.API_SECRET)
-//     .update(method + path + expires + postBody)
-//     .digest('hex');
-
-//   let headers = {
-//     'content-type': 'application/json',
-//     Accept: 'application/json',
-//     'X-Requested-With': 'XMLHttpRequest',
-//     'api-expires': expires,
-//     'api-key': process.env.API_KEY,
-//     'api-signature': signature
-//   };
-//   //console.log(`https://${true ? "testnet" : "www"}.bitmex.com${path}`);
-//   return {
-//     headers: headers,
-//     url: `https://${
-//       process.env.TESTNET == 'true' ? 'testnet' : 'www'
-//     }.bitmex.com${path}`,
-//     method: method,
-//     body: postBody
-//   };
-// }
+// API_KEY = "GAhaeLn6Hmw__7vuNOThm3AE"
+// API_SECRET = "dNJMIUA2v0mb-fggdMWNJrocCYHtpmcIXWHi2uQ_qrBQorDv"
+// path='order/bulk', postdict={'orders': orders}, verb='POST'
 
 // function orderBulk(orders) {
 //   return putResponse(orders, '/api/v1/order/bulk', 'POST');
