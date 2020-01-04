7ee778d7015af589d6db850b2a81ee81bb2bf557
Effanuel <32594043+Effanuel@users.noreply.github.com>
2020-01-04 15:57:22 +0200

    Removed ccxt comments

--- api/src/controllers/rangeToolAPI.ts
+++ api/src/controllers/rangeToolAPI.ts
@@ -1,35 +1,6 @@
-// const request = require('request');
-// const crypto = require('crypto');
-// import * as ccxt from "ccxt";
 import { Request, Response } from "express";
 import { _curl_bitmex } from "../util/auth";
-
-// const exchangeId = "bitmex",
-//   exchangeClass = ccxt[exchangeId],
-//   exchange = new exchangeClass({
-//     apiKey: process.env.API_KEY,
-//     secret: process.env.API_SECRET,
-//     timeout: 30000,
-//     enableRateLimit: process.env.ENABLE_RATE_LIMIT == "true"
-//   });
-// if (process.env.TESTNET == "true") {
-//   exchange.urls["api"] = exchange.urls["test"];
-// }
-
-// const errorHandling = (e: any, res: any) => {
-//   const errorMessage = JSON.parse(e.message.slice(7)).error.message;
-
-//   if (e instanceof ccxt.NetworkError) {
-//     return res.status(500).json({ errorMessage });
-//   } else if (e instanceof ccxt.ExchangeError) {
-//     return res.status(500).json({ errorMessage });
-//   } else if (e instanceof ccxt.RequestTimeout) {
-//     //validation error *
-//     return res.status(500).json({ errorMessage });
-//   } else {
-//     return res.status(500).json({ errorMessage: "ERROR" });
-//   }
-// };
+import { logger } from "../util/logger";
 
 /**
  * Bulk order POST request to the exchange API
@@ -38,7 +9,7 @@ export const postOrder = async (req: Request, res: Response, next: any) => {
   try {
     // const response = await exchange.privatePostOrderBulk(req.body);
     const response = await _curl_bitmex("order/bulk", req.body, "POST");
-    console.log("POST", response);
+    logger.info("Successful POST request (postOrder)");
     return res.send({ success: res.statusCode });
   } catch (error) {
     return res.status(400).send({ error: error });
@@ -92,10 +63,3 @@ export const postOrder = async (req: Request, res: Response, next: any) => {
 //     return res.send({ instruments: instrumentsArray });
 //   });
 // };
-// API_KEY = "GAhaeLn6Hmw__7vuNOThm3AE"
-// API_SECRET = "dNJMIUA2v0mb-fggdMWNJrocCYHtpmcIXWHi2uQ_qrBQorDv"
-// path='order/bulk', postdict={'orders': orders}, verb='POST'
-
-// function orderBulk(orders) {
-//   return putResponse(orders, '/api/v1/order/bulk', 'POST');
-// }
