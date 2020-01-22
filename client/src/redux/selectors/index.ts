560a2f12647adde03f8cd05f5ea138fda00c3a44
Effanuel <32594043+Effanuel@users.noreply.github.com>
2020-01-22 15:18:40 +0200

    Added risk selector

--- client/src/redux/selectors/index.ts
+++ client/src/redux/selectors/index.ts
@@ -1,15 +1,27 @@
 import { createSelector } from "reselect";
 import { AppState } from "../models/state";
-
+// PREVIEW ACTIONS
 const getShowPreview = (state: AppState) => state.preview.showPreview;
-const getError = (state: AppState) => state.preview.error;
 const getOrders = (state: AppState) => state.preview.orders;
-
+const getOrderLoading = (state: AppState) => state.preview.loading;
+// WEBSOCKET ACTIONS
 const getWsSymbol = (state: AppState) => state.websocket.symbol;
 const table_instrument = (state: AppState) => state.websocket.instrument;
+const table_order = (state: AppState) => state.websocket.order;
 const websocketLoading = (state: AppState) => state.websocket.loading;
 const websocketMessage = (state: AppState) => state.websocket.message;
 const websocketConnected = (state: AppState) => state.websocket.connected;
+
+export const orderLoadingSelector = createSelector(
+  [getOrderLoading],
+  loading => loading
+);
+
+export const websocketOrder = createSelector([table_order], orders => {
+  if (orders[0]) {
+    return orders[0].orderID;
+  }
+});
 
 export const showPreviewSelector = createSelector(
   [getShowPreview],
@@ -23,11 +35,6 @@ export const messageSelector = createSelector(
   [websocketMessage],
   message => message
 );
-
-export const errorSelector = createSelector([getError], error => {
-  console.log(error);
-  return error;
-});
 
 export const websocketCurrentPrice = createSelector(
   [table_instrument, getWsSymbol],
@@ -43,13 +50,17 @@ export const websocketCurrentPrice = createSelector(
 
 export const ordersAveragePriceSelector = createSelector(
   [getOrders, getShowPreview],
-  (orderList: [], previewTable): number | void => {
+  (orderList = [], previewTable): number | void => {
     if (previewTable) {
-      const total_quantity = orderList.reduce(
+      // We push stop-loss order in /utils distribution functions
+      // so we want to exclude it here.
+
+      //
+      const total_quantity = orderList.orders.reduce(
         (total: number, n: any): number => total + n.orderQty,
         0
       );
-      const contract_value = orderList.reduce(
+      const contract_value = orderList.orders.reduce(
         (total: number, n: any): number => total + n.orderQty / n.price,
         0
       );
@@ -59,6 +70,21 @@ export const ordersAveragePriceSelector = createSelector(
 );
 
 export const ordersSelector = createSelector([getOrders], orders => orders);
+
+export const ordersRiskSelector = createSelector(
+  [getOrders, ordersAveragePriceSelector, getShowPreview],
+  (orderList = {}, averageEntry, previewTable): number | void => {
+    if (previewTable && averageEntry > 0 && averageEntry && orderList["stop"]) {
+      let quantity = orderList.stop.orderQty;
+      // 1 contract of ETH is for 0.01 mXBT which is 1e-6 XBT
+      if (orderList.stop["symbol"] === "ETHUSD")
+        quantity *= 1e-6 * averageEntry ** 2;
+      const entryValue = quantity / averageEntry;
+      const exitValue = quantity / orderList.stop["stopPx"] || 1;
+      return Math.abs(parseFloat((entryValue - exitValue).toFixed(5)));
+    }
+  }
+);
 
 export const websocketLoadingSelector = createSelector(
   [websocketLoading],
