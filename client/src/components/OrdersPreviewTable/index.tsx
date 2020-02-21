04e569982aa56ed8c6c5dae2091ce7d4d0b80959
Effanuel <32594043+Effanuel@users.noreply.github.com>
2020-02-21 17:31:02 +0200

    Fixed formating

--- client/src/components/OrdersPreviewTable/index.tsx
+++ client/src/components/OrdersPreviewTable/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useSelector, shallowEqual } from "react-redux";
 import { ordersSelector } from "../../redux/selectors";
 // UTILS
-import cx from 'classnames'
+import cx from "classnames";
 import styles from "./styles.module.css";
 import { AppState } from "../../redux/models/state";
 
@@ -18,8 +18,8 @@ export default function OrdersPreviewTable(props: Props) {
   );
 
   // This code needs fixing...
-  const x_side = orders.orders[0].side === "Sell"
-  const y_side = x_side ? "Buy": "Sell"
+  const x_side = orders.orders[0].side === "Sell";
+  const y_side = x_side ? "Buy" : "Sell";
 
   let sideStyle = cx({
     [styles.side_color__sell]: x_side,
@@ -37,10 +37,11 @@ export default function OrdersPreviewTable(props: Props) {
     [styles.side__sell]: !x_side,
     [styles.side__buy]: x_side
   });
-  
-  function reformat(number: any): string{
-    // reformats 1234567 to 1,234,567
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+
+  function reformat(num: any) {
+    return num.toString().replace(/^[+-]?\d+/, function(int: any) {
+      return int.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+    });
   }
 
   return (
@@ -55,34 +56,21 @@ export default function OrdersPreviewTable(props: Props) {
       <tbody>
         {orders.orders.map((x: any, i: number) => {
           return (
-            <tr
-              key={`${i}`}
-              className={sideStyle}
-            >
-              <td key={x * i + "a"}>
-                {reformat(x.orderQty)}
-              </td>
+            <tr key={`${i}`} className={sideStyle}>
+              <td key={x * i + "a"}>{reformat(x.orderQty)}</td>
               <td key={x * i + "b"} className={sideText_x}>
                 {x.side}
               </td>
-              <td key={x * i + "c"}>
-                {reformat(x.price)}
-              </td>
+              <td key={x * i + "c"}>{reformat(x.price)}</td>
             </tr>
           );
         })}
 
         {orders.stop.stopPx ? (
-          <tr
-            className={sideStyle_stop}
-          >
-            <td>
-              {reformat(orders.stop.orderQty)}
-            </td>
+          <tr className={sideStyle_stop}>
+            <td>{reformat(orders.stop.orderQty)}</td>
             <td className={sideText_y}>{y_side}</td>
-            <td>
-              {reformat(orders.stop.stopPx)}
-            </td>
+            <td>{reformat(orders.stop.stopPx)}</td>
           </tr>
         ) : null}
       </tbody>
