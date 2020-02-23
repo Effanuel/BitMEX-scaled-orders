d0f6ff03795334e0a833bd428b2d8bff9c5d9482
Effanuel <32594043+Effanuel@users.noreply.github.com>
2020-02-23 22:23:44 +0200

    More versatile

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -4,19 +4,37 @@ import cx from "classnames";
 import styles from "./styles.module.css";
 
 interface Props {
+  id?: string;
   children: React.ReactNode;
-  variant?: "submit" | "text";
+  variant?: "submit" | "text" | "custom";
   disabled?: boolean;
   onClick: (event: any) => void;
+  style?: any;
+  className?: any;
 }
 
-function Button({ variant = "submit", children, disabled, onClick }: Props) {
+function Button({
+  id,
+  variant = "submit",
+  children,
+  disabled,
+  onClick,
+  style,
+  className = ""
+}: Props) {
   const buttonStyle = cx({
     [styles.button]: variant === "submit",
-    [styles.text_button]: variant === "text"
+    [styles.text_button]: variant === "text",
+    [className]: variant === "custom"
   });
   return (
-    <button className={buttonStyle} disabled={disabled} onClick={onClick}>
+    <button
+      id={id}
+      className={buttonStyle}
+      style={style}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
