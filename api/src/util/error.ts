de409109eda2839189a7da6a43ac89aed1909924
Effanuel <32594043+Effanuel@users.noreply.github.com>
2020-03-15 00:40:44 +0200

    Updated Error handler

--- api/src/util/error.ts
+++ api/src/util/error.ts
@@ -1,5 +1,34 @@
 import { logger } from "./logger";
 
+const error_400_handler = (response_message: string): string => {
+  const error_messages: any = {
+    "duplicate clordid": "Duplicate order.",
+    "insufficient available balance": "Insufficient funds.",
+    "missing api key": "Missing API key.",
+    spam: "Spam: quantity is too low.",
+    "immediate liquidation": "Executing would lead to immediate liquidation."
+  };
+
+  // return response_message.includes("duplicate clordid")
+  // ? "Duplicate order."
+  // : response_message.includes("insufficient available balance")
+  // ? "Insufficient funds."
+  // : response_message.includes("missing api key")
+  // ? "Missing API key."
+  // : response_message.includes("spam")
+  // ? "Spam: quantity is too low."
+  // : response_message.includes("immediate liquidation")
+  // ? "Executing would lead to immediate liquidation."
+  // : "Unhandled Error";
+
+  for (const match in error_messages) {
+    if (response_message.includes(match)) {
+      return error_messages[match];
+    }
+  }
+  return "Unhandled Error";
+};
+
 export const ErrorHandler = (error: any) => {
   logger.debug(error.message);
   const errorMessage = ((statusCode: number): string => {
@@ -17,17 +46,7 @@ export const ErrorHandler = (error: any) => {
         const response_message = response_error
           ? response_error.message.toLowerCase()
           : "";
-        if (response_message.includes("duplicate clordid")) {
-          return "Duplicate order";
-        } else if (
-          response_message.includes("insufficient available balance")
-        ) {
-          return "Insufficient funds.";
-        } else if (response_message.includes("missing api key")) {
-          return "Missing API key.";
-        } else if (response_message.includes("spam")){
-          return "Spam: quantity is too low.";
-        }
+        return error_400_handler(response_message);
 
       default:
         return "Error::Default";
@@ -37,28 +56,43 @@ export const ErrorHandler = (error: any) => {
   return errorMessage;
 };
 
-// if (error.statusCode == 401) {
-//     throw "API Key or Secret incorrect, please check and restart.";
-//   } else if (error.statusCode == 404) {
-//     if (verb == "DELETE") {
-//       throw "Order to delete not found";
-//     }
-//     throw "Unable to contact the BitMEX API (404).";
-//   } else if (error.statusCode == 429) {
-//     throw "Ratelimited on current request";
-//   } else if (error.statusCode == 503) {
-//     throw "Unable to contact BitMEX";
-//   } else if (error.statusCode == 400) {
-//     const response_error = JSON.parse(error.error).error;
-//     const response_message = response_error
-//       ? response_error.message.toLowerCase()
-//       : "";
-//     if (response_message.includes("duplicate clordid")) {
-//       throw "Duplicate";
-//     } else if (response_message.includes("insufficient available balance")) {
-//       throw "insufficient funds";
-//     }
-//     console.log(error);
+// import { logger } from "./logger";
 
-//     throw "Error";
-//   }
+// export const ErrorHandler = (error: any) => {
+//   logger.debug(error.message);
+//   const errorMessage = ((statusCode: number): string => {
+//     switch (statusCode) {
+//       case 401:
+//         return "API Key or Secret incorrect, please check and restart.";
+//       case 404:
+//         return "Unable to contact the BitMEX API (404).";
+//       case 429:
+//         return "Ratelimited on current request";
+//       case 503:
+//         return "Unable to contact BitMEX";
+//       case 400:
+//         const response_error = JSON.parse(error.error).error;
+//         const response_message = response_error
+//           ? response_error.message.toLowerCase()
+//           : "";
+//         if (response_message.includes("duplicate clordid")) {
+//           return "Duplicate order";
+//         } else if (
+//           response_message.includes("insufficient available balance")
+//         ) {
+//           return "Insufficient funds.";
+//         } else if (response_message.includes("missing api key")) {
+//           return "Missing API key.";
+//         } else if (response_message.includes("spam")) {
+//           return "Spam: quantity is too low.";
+//         } else if (response_message.includes("immediate liquidation")) {
+//           return "Executing would lead to immediate liquidation.";
+//         }
+
+//       default:
+//         return "Error::Default";
+//     }
+//   })(error.statusCode);
+//   logger.log("error", errorMessage);
+//   return errorMessage;
+// };
