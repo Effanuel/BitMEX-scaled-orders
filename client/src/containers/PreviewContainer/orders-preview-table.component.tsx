ac52b5c7c8645d9b6bc1be62a225ca202ff39595
Juozas <juozasr@wix.com>
2020-07-02 20:43:41 +0300

    Uses new price formatting helper

--- client/src/containers/PreviewContainer/orders-preview-table.component.tsx
+++ client/src/containers/PreviewContainer/orders-preview-table.component.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-// REDUX
 import {useSelector, shallowEqual} from 'react-redux';
-import {AppState} from 'redux/models/state';
-// STYLES
 import cx from 'classnames';
-import styles from './orders-preview-table.module.css';
 
-function OrdersPreviewTable() {
+import {AppState} from 'redux/models/state';
+import styles from './orders-preview-table.module.css';
+import {formatPrice} from 'general/formatting';
+
+const generateSideColorStyles = (xSide: boolean) => ({
+  normal: cx({
+    [styles.side_color__sell]: xSide,
+    [styles.side_color__buy]: !xSide,
+  }),
+  stop: cx({
+    [styles.side_stop__sell]: !xSide,
+    [styles.side_stop__buy]: xSide,
+  }),
+  textX: cx({
+    [styles.side__sell]: xSide,
+    [styles.side__buy]: !xSide,
+  }),
+  textY: cx({
+    [styles.side__sell]: !xSide,
+    [styles.side__buy]: xSide,
+  }),
+});
+
+export default function OrdersPreviewTable() {
   const {orders} = useSelector(
     (state: AppState) => ({
       orders: state.preview.orders,
@@ -14,53 +33,31 @@ function OrdersPreviewTable() {
     shallowEqual,
   );
 
-  // Handles side border colors
-  const x_side = orders?.orders?.[0].side === 'Sell';
-  const y_side = x_side ? 'Buy' : 'Sell';
-
-  const sideStyle = cx({
-    [styles.side_color__sell]: x_side,
-    [styles.side_color__buy]: !x_side,
-  });
-  const sideStyle_stop = cx({
-    [styles.side_stop__sell]: !x_side,
-    [styles.side_stop__buy]: x_side,
-  });
-  const sideText_x = cx({
-    [styles.side__sell]: x_side,
-    [styles.side__buy]: !x_side,
-  });
-  const sideText_y = cx({
-    [styles.side__sell]: !x_side,
-    [styles.side__buy]: x_side,
-  });
+  const xSide = orders?.orders?.[0].side === 'Sell';
+  const ySide = xSide ? 'Buy' : 'Sell';
+  const {normal, stop, textX, textY} = generateSideColorStyles(xSide);
 
   function renderOrders() {
-    return orders.orders.map((x, i) => (
-      <tr key={`${i}`} className={sideStyle}>
-        <td key={i + 'a'}>{reformat(x.orderQty)}</td>
-        <td key={i + 'b'} className={sideText_x}>
-          {x.side}
+    return orders.orders.map((order, i) => (
+      <tr key={`${i}`} className={normal}>
+        <td key={i + 'a'}>{formatPrice(order.orderQty)}</td>
+        <td key={i + 'b'} className={textX}>
+          {order.side}
         </td>
-        <td key={i + 'c'}>{reformat(x.price)}</td>
+        <td key={i + 'c'}>{formatPrice(order.price)}</td>
       </tr>
     ));
   }
+
   function renderStop() {
     return orders.stop.stopPx ? (
-      <tr className={sideStyle_stop}>
-        <td>{reformat(orders.stop.orderQty)}</td>
-        <td className={sideText_y}>{y_side}</td>
-        <td>{reformat(orders.stop.stopPx)}</td>
+      <tr className={stop}>
+        <td>{formatPrice(orders.stop.orderQty)}</td>
+        <td className={textY}>{ySide}</td>
+        <td>{formatPrice(orders.stop.stopPx)}</td>
       </tr>
     ) : null;
   }
-
-  // .details_container {
-  //   border: 1px solid grey;
-  //   border-radius: 2px;
-  //   box-shadow: 3px 3px 5px black;
-  // }
 
   return (
     <table className={styles.table}>
@@ -77,13 +74,4 @@ function OrdersPreviewTable() {
       </tbody>
     </table>
   );
-}
-export {OrdersPreviewTable};
-
-// Reformats numbers
-// ex. 123456.7890 => 123,456.7890
-function reformat(num: any) {
-  return num.toString().replace(/^[+-]?\d+/, function (int: any) {
-    return int.replace(/(\d)(?=(\d{3})+$)/g, '$1,');
-  });
 }
