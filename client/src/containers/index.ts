86a5d7fb3718c3680336732cb19b71755f097188
Juozas <juozasr@wix.com>
2020-09-03 21:15:46 +0300

    Misc

--- client/src/containers/index.ts
+++ client/src/containers/index.ts
@@ -1,4 +1,4 @@
-export {default as ScaledOrders} from './ScaledOrders/scaled.container';
+export {default as ScaledOrders} from './ScaledOrders/ScaledOrders';
 export {default as MarketOrderContainer} from './MarketOrder';
 export {default as TrailingLimitOrder} from './TrailingLimitOrder/TrailingLimitOrderContainer';
 export {default as TickerPricesContainer} from './TickerPrices/TickerPricesContainer';
