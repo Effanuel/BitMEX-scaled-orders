6288ec08b1dc22f4d95786cdcd10221b8d5950fa
Juozas <juozasr@wix.com>
2020-11-02 02:08:03 +0200

    Added mocking of notification middleware

--- client/src/tests/mockStore.ts
+++ client/src/tests/mockStore.ts
@@ -5,6 +5,7 @@ import {MockBitMEX_API} from './mockAPI';
 import {rootReducer} from 'redux/store';
 import createStore from './configStore';
 import {AppState} from 'redux/models/state';
+import notificationMiddleware from '../redux/middlewares/notification';
 
 export const mockedDefaultState: AppState = {
   websocket: mockWebsocketState({}),
@@ -15,7 +16,8 @@ export const mockedDefaultState: AppState = {
 
 export function createMockedStore(overrideState: Partial<AppState> = {}, mockAPI = new MockBitMEX_API()) {
   const preloadedState = {...mockedDefaultState, ...overrideState};
-  const enhancer = compose(applyMiddleware(thunk.withExtraArgument(mockAPI)));
+  const middlewares = [thunk.withExtraArgument(mockAPI), notificationMiddleware];
+  const enhancer = compose(applyMiddleware(...middlewares));
 
   return createStore(rootReducer, preloadedState, enhancer);
 }
