889c52374a070363cff6f3104acefb79135a094f
Juozas <juozasr@wix.com>
2021-03-13 14:40:38 +0200

    Revert "See the problem with ci"
    This reverts commit 3cdb4e2bad2ace3003144e232e724caa22e62a10.

--- client/src/containers/CrossOrder/CrossOrderContainer.spec.ts
+++ client/src/containers/CrossOrder/CrossOrderContainer.spec.ts
@@ -95,7 +95,15 @@ describe('CrossOrderContainer', () => {
       .inspect({actions: storeActions(), cross: getState('cross')});
 
     expect(result).toEqual({
-      actions: [],
+      actions: [
+        'REDUX_WEBSOCKET::OPEN',
+        'REDUX_WEBSOCKET::MESSAGE',
+        'cross/CREATE_CROSS_ORDER',
+        'cross/ORDER_CROSSED_ONCE',
+        'REDUX_WEBSOCKET::MESSAGE',
+        'cross/CROSS_POST_MARKET_ORDER/pending',
+        'cross/CROSS_POST_MARKET_ORDER/fulfilled',
+      ],
       api: [{marketOrder: {orderQty: 200, side: 'Sell', symbol: 'XBTUSD'}}],
       cross: {
         crossOrderPrice: 0,
