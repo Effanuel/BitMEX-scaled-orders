89ed0dd0cae711024890750c006d3315f8214d54
Juozas Rimantas <juozasr@wix.com>
2021-07-20 00:14:01 +0300

    Fix test

--- client/src/general/hooks.ts
+++ client/src/general/hooks.ts
@@ -1,12 +1,5 @@
 import React from 'react';
 import {ModalContext} from 'context/modal-context';
-import {AnyAction, Dispatch} from 'redux';
-
-export function useSimpleDispatch(dispatch: Dispatch, func: () => AnyAction) {
-  return React.useCallback(() => {
-    dispatch(func());
-  }, [dispatch, func]);
-}
 
 export function useModal() {
   const context = React.useContext(ModalContext);
